9829c8b557cbece851183411283915b15d9ee7fa
Glenn Siegman <gsiegman@gmail.com>
2009-11-26 04:38:56 -0600

    modified settings to include PROJECT_ROOT and updated TEMPLATE_DIRS accordingly

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -1,5 +1,9 @@
 # Django settings for sample_project project.
 
+import os.path
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -69,6 +73,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
